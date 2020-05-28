29c4d764a45a1ec34d895b09bf9aff1cf4797618
ptabasso2 <pejman.tabassomi@gmail.com>
2020-05-29 01:02:38 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# DogStatsD java client example
+
+
+
+A very basic example of DogStatsD usage with Java. This assumes the following prerequsites: 
++ You already have a JDK installed (ex: OpenJDK 11)
++ You already have a Datadog agent running.
+  
+
+### _Preliminary tasks_
+
+
+**Building the application**
+
+```
+COMP10619:pejman.tabassomi$ ./gradlew build
+```
+
+
+
+### _Run the tests_
+
+Open a new terminal window and run the following command **multiple times**:
+
+```
+COMP10619:Kafka pejman.tabassomi$ java -jar build/libs/statsdexample-1.0-SNAPSHOT.jar
+Counter iteration: 0
+Counter iteration: 1
+Counter iteration: 2
+Counter iteration: 3
+Counter iteration: 4
+Counter iteration: 5
+Counter iteration: 6
+Counter iteration: 7
+Counter iteration: 8
+Counter iteration: 9
+Gauge iteration: 0
+Gauge iteration: 1
+Gauge iteration: 2
+...
+```
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
+import com.timgroup.statsd.StatsDClient;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+        StatsDClient Statsd = new NonBlockingStatsDClientBuilder()
+                .prefix("statsd")
+                .hostname("localhost")
+                .port(8125)
+                .build();
+
+        // Increment counter
+        for (int i = 0; i < 10; i++) {
+            Statsd.incrementCounter("example_metric.increment", new String[]{"environment:dev"});
+            Statsd.decrementCounter("example_metric.decrement", new String[]{"environment:dev"});
+            Statsd.count("example_metric.count", 2, new String[]{"environment:dev"});
+            System.out.println("Counter iteration: " + i);
+            Thread.sleep(5000);
+        }
+
+        // Emit  gauge
+        for (int i = 0; i < 10; i++) {
+            Statsd.recordGaugeValue("example_metric.gauge", i, new String[]{"environment:dev"});
+            System.out.println("Gauge iteration: " + i);
+            Thread.sleep(10000);
+        }
+
+
+        // Submit Histogram
+        for (int i = 0; i < 10; i++) {
+            Statsd.recordHistogramValue("example_metric.histogram", new Random().nextInt(20), new String[]{"environment:dev"});
+            System.out.println("Histogram iteration: " + i);
+            Thread.sleep(2000);
+        }
+
+        // Record Distribution
+        for (int i = 0; i < 10; i++) {
+            Statsd.recordDistributionValue("example_metric.distribution", new Random().nextInt(20), new String[]{"environment:dev"});
+            System.out.println("Distribution iteration: " + i);
+            Thread.sleep(2000);
+        }
+
+        System.exit(0);
+
+    }
+}
